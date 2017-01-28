621759b38aa310e24fe522bc1fad1f9321ab8eb5
starlightcandy <uta9784@gmail.com>
2017-01-28 03:15:52 -0500

    Translucent demo update

---
+++ tc_res/tc_script.js
@@ -0,0 +1,118 @@
+/* 
+ * TransparentCard design demo    
+ * Sanggyeong Jo                    
+ * Jan 24. 2017                     
+ *                                  
+ * Contacts                         
+ * Github : github.com/byeolbit     
+ * Email : info@byeolbit.com        
+ *         sanggyeong.jo@gmail.com
+ *
+ * You can find this project at https://github.com/byeolbit/transparentCard
+ */  
+
+$(document).ready(function(){
+    var $demoCard = $('#cw'),
+        background = '.tl-contents',
+        $targetBg = $(background),
+        filterValue = 5,
+        cardColor = 'white',
+        draggable = true,
+        shadow = true,
+        bgAttach;
+
+    var $card = $demoCard.children('.tl-card'),
+        $cardBg = $demoCard.children('.tl-card-bg-container').
+                            children('.tl-card-bg');
+
+    var $bBg = $('#bBg'),
+        $bBlur = $('#bBlur'),
+        $bFix = $('#bFix'),
+        $bTog = $('#bTog'),
+        $bNum = $('#bNum');
+
+    var bgImages = ['url("img/bg1.jpg")',
+                    'url("img/bg2.jpg")',
+                    'url("img/bg3.jpg")',
+                    'url("img/bg4.jpg")',
+                    'url("img/bg5.jpg")',
+                    'url("img/bg6.jpg")'
+                    ];
+
+    if( $targetBg.css('background-attachment') == 'fixed' )
+        bgAttach = true;
+    else
+        bgAttach = false;
+
+    $demoCard.position({
+        my: 'center',
+        at: 'center',
+        of: '#bgEl'
+    });
+    applyOption($demoCard, background,
+                filterValue, cardColor, draggable, shadow);
+
+    $bNum.attr('placeholder',filterValue);
+
+    $bBlur.click(function(){
+        filterValue = $bNum.val();
+        applyOption($demoCard, background,
+                    filterValue, cardColor, draggable, shadow);
+    });
+
+    $bBg.click(function(){
+        var t = $targetBg.css('background-attachment');
+        $targetBg.css('background',
+                      bgImages[Math.floor((Math.random() * 10))%6]);
+        $targetBg.css('background-attachment',t);
+        applyOption($demoCard, background,
+                    filterValue, cardColor, draggable, shadow);
+    });
+
+    $bFix.click(function(){
+        if (!bgAttach){
+            //var s = $targetBg.css('background-position');
+            $targetBg.css('background-attachment','fixed');
+            $cardBg.css('background-position','0px 0px');
+            $bFix.text('bgFixOff');
+        }
+        else {
+            $targetBg.css('background-attachment','inherit');
+            $bFix.text('bgFixOn');
+        }
+        
+        bgAttach = !bgAttach;
+        
+        applyOption($demoCard, background,
+                    filterValue, cardColor, draggable, shadow);
+    });
+
+    var $cb1 = $('#cb1'),
+        $cb2 = $('#cb2'),
+        $cb3 = $('#cb3'),
+        $cb4 = $('#cb4'),
+        $cc = $('.tl-card-contents');
+
+    $cb1.click(function(){
+        $cc.css('background-color','rgba(255,255,255,0.4)');
+    });
+    $cb2.click(function(){
+        $cc.css('background-color','rgba(120,120,120,0.4)');
+    });
+    $cb3.click(function(){
+        $cc.css('background-color','rgba(30,30,30,0.7)');
+    });
+    $cb4.click(function(){
+        $cc.css('background-color','rgba(0,0,0,0)');
+    });
+
+});
+
+function applyOption($dc, bg, fv, cc, d, s){
+    $dc.translucent(bg,{
+        filterValue : fv,
+        cardColor : cc,
+        draggable : d,
+        shadow : s
+    });
+}
